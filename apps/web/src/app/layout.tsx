87bbd1a25b6e6e0b33a6a927d80453d410f23c76
buri1126 <koutarouhanabusa@gmail.com>
2025-11-25 20:39:15 +0900

    providerを設定する

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,45 +1,48 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../index.css";
-import Header from "@/components/header";
 import Providers from "@/components/providers";
+import { UIProvider } from "@mySlides/ui";
 
 const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-	display: "swap",
-	preload: false,
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+  display: "swap",
+  preload: false,
 });
 
 const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
-	display: "swap",
-	preload: false,
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+  display: "swap",
+  preload: false,
 });
 
 export const metadata: Metadata = {
-	metadataBase: new URL(
-		process.env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:3001",
-	),
-	title: "mySlides",
-	description: "mySlides",
+  metadataBase: new URL(
+    process.env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:3001"
+  ),
+  title: "mySlides",
+  description: "mySlides",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				<Providers>
-					<div className="grid h-svh grid-rows-[auto_1fr]">{children}</div>
-				</Providers>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
+      >
+        <UIProvider>
+          <Providers>
+            <div className="grid h-svh grid-rows-[auto_1fr]">{children}</div>
+          </Providers>
+        </UIProvider>
+      </body>
+    </html>
+  );
 }
