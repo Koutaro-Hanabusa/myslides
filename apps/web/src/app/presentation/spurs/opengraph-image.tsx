cf1d8d9d634624b193636cff2fd0883f8ac659ad
buri1126 <koutarouhanabusa@gmail.com>
2025-11-25 22:46:39 +0900

    edge指定を消す

--- apps/web/src/app/presentation/spurs/opengraph-image.tsx
+++ apps/web/src/app/presentation/spurs/opengraph-image.tsx
@@ -2,8 +2,6 @@ import {
 	generateOGPImage,
 	gradientPresets,
 } from "@/lib/ogp/generate-image";
-
-export const runtime = "edge";
 export const alt = "Welcome to spurs";
 export const size = {
 	width: 1200,
