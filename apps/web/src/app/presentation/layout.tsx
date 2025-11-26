37ce7d868e02213c4f8aa81379888b229b58a650
buri1126 <koutarouhanabusa@gmail.com>
2025-11-26 21:03:17 +0900

    fullscreen時に拡大されてしまう問題を解決

--- apps/web/src/app/presentation/layout.tsx
+++ apps/web/src/app/presentation/layout.tsx
@@ -4,7 +4,18 @@ export default function PresentationLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<div style={{ width: "100%", height: "100vh", overflow: "hidden" }}>
+		<div
+			style={{
+				width: "100%",
+				height: "100vh",
+				overflow: "hidden",
+				position: "fixed",
+				top: 0,
+				left: 0,
+				right: 0,
+				bottom: 0,
+			}}
+		>
 			{children}
 		</div>
 	);
