b3f3bbb7521fc3c3f03217f0bbcff8ac455ed9ca
buri1126 <koutarouhanabusa@gmail.com>
2025-12-03 22:07:12 +0900

    contain反映忘れ

--- apps/web/src/app/better-t-stack/slides/content.tsx
+++ apps/web/src/app/better-t-stack/slides/content.tsx
@@ -4,7 +4,7 @@ export default function Content() {
 			{/* 見出しスライド */}
 			<section
 				data-background-image="https://pub-12dea38316b14a799f73d17465eadeb1.r2.dev/外部登壇資料テンプレ/千_外部登壇スライド_見出し.png"
-				data-background-size="cover"
+				data-background-size="contain"
 			>
 				{/* 見出しコンテンツをここに追加 */}
 			</section>
@@ -12,7 +12,7 @@ export default function Content() {
 			{/* 本文スライド */}
 			<section
 				data-background-image="https://pub-12dea38316b14a799f73d17465eadeb1.r2.dev/外部登壇資料テンプレ/千_外部登壇スライド_本文.png"
-				data-background-size="cover"
+				data-background-size="contain"
 			>
 				{/* 本文コンテンツをここに追加 */}
 			</section>
--- apps/web/src/app/better-t-stack/slides/cover.tsx
+++ apps/web/src/app/better-t-stack/slides/cover.tsx
@@ -2,21 +2,18 @@ export default function Cover() {
   return (
     <section
       data-background-image="https://pub-12dea38316b14a799f73d17465eadeb1.r2.dev/外部登壇資料テンプレ/千_外部登壇スライド_表紙.png"
-      data-background-size="cover"
+      data-background-size="contain"
     >
-      <div className="text-left">
-        <span>Cloudflare Meet-up Tokyo Vol.9</span>
-        <h1>
+      <div className="text-left pt-[5%]">
+        <span className="text-[1.2em]">Cloudflare Meet-up Tokyo Vol.9</span>
+        <h1 className="text-[2.5em]">
           より良い技術スタックで
           <br />
           cloudflareにデプロイしよう
         </h1>
-        <a
-          href="https://x.com/buri16_koutaro"
-          className="block pt-64 text-blue-500"
-        >
+        <span className="block pt-[20%] text-[1em] text-blue-500">
           ぶりお @burio_16
-        </a>
+        </span>
       </div>
     </section>
   );
