2877154ba755266353c94911a6b0177d3591552e
buri1126 <koutarouhanabusa@gmail.com>
2025-12-13 23:58:02 +0900

    r２のパスが違う

--- apps/web/src/app/revealjs-slideDeck/slides/content.tsx
+++ apps/web/src/app/revealjs-slideDeck/slides/content.tsx
@@ -1,7 +1,7 @@
 export default function Content() {
 	return (
 		<section
-			data-background-image="https://pub-12dea38316b14a799f73d17465eadeb1.r2.dev/content.png"
+			data-background-image="https://pub-12dea38316b14a799f73d17465eadeb1.r2.dev/buroSlide/content.png"
 			data-background-size="contain"
 		>
 			{/* コンテンツをここに追加 */}
