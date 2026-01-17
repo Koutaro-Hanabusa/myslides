6715e1249461413c6560e1771183c08cb38da93b
buri1126 <koutarouhanabusa@gmail.com>
2026-01-17 23:19:50 +0900

    表紙を編集

--- apps/web/src/app/you-must-have-dotfiles/slides/cover.tsx
+++ apps/web/src/app/you-must-have-dotfiles/slides/cover.tsx
@@ -7,7 +7,19 @@ export default function Cover() {
 			data-background-image={`${R2_BASE}/${TEMPLATE_PATH}/千_外部登壇スライド_表紙.png`}
 			data-background-size="contain"
 		>
-			<h1>you-must-have-dotfiles</h1>
+			<div className="text-left">
+				<h3>
+					Yoriai.cafe 二日間限定オープン！ 🍛ホタテカレーと出会いの「寄り合い」
+				</h3>
+				<br />
+				<h1 className="leading-tight">
+					Vim使いでなくても
+					<br />
+					dotfilesを管理しよう
+				</h1>
+				<br />
+				<h3>ぶりお @burio_16</h3>
+			</div>
 		</section>
 	);
 }
